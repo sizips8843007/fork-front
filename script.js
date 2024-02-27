5b324837bb83e31928f3d2e35890ee43cdae01b1
sizips8843007 <130811729+sizips8843007@users.noreply.github.com>
2024-02-27 11:44:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function init() {
 };
 
 async function getCode(questCode) {
-  const Url = `generate`;//클라우드 타입 서버 주소 입력
+  const Url = `https://port-0-fork-back-am952nlt3rdsez.sel5.cloudtype.app/generate`;//클라우드 타입 서버 주소 입력
   const Data = JSON.stringify({ userInput: questCode });   
   let response; 
     try {
